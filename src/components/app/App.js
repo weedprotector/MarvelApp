fd8595a94fb21113834a6e7259d096141f5a75ed
Mikhail <weedprotector@gmail.com>
2022-10-23 16:19:29 +0500

    separating components to different files

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,31 +1,10 @@
-import { useState } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import AppHeader from "../appHeader/AppHeader";
-import RandomChar from "../randomChar/RandomChar";
-import CharList from "../charList/CharList";
-import CharInfo from "../charInfo/CharInfo";
-import ErrorBoundary from "../errorBoundary/ErrorBoundary";
-import ComicsList from "../comicsList/ComicsList";
-import AppBanner from "../appBanner/AppBanner"
-
-import decoration from '../../resources/img/vision.png';
-
+import MainPage from "../pages/MainPage";
+import ComicsPage from "../pages/ComicsPage";
 
 const App = () => {
-
-    const [selectedChar, setChar] = useState(null);
-    const [selectedComics, setComics] = useState(null);
-
-
-    const onCharSelected = (id) => {
-        setChar(id)
-    }
-
-    const onComicsSelected = (id) => {
-        setComics(id)
-    }
-
 
     return (
         <Router>
@@ -34,22 +13,10 @@ const App = () => {
                 <main>
                     <Switch>
                         <Route exact path="/MarvelApp">
-                            <ErrorBoundary>
-                                <RandomChar/>
-                            </ErrorBoundary>
-                            <div className="char__content">
-                                <ErrorBoundary>
-                                    <CharList onCharSelected={onCharSelected} selectedId={selectedChar}/>
-                                </ErrorBoundary>
-                                <ErrorBoundary>
-                                    <CharInfo charId={selectedChar}/>
-                                </ErrorBoundary>
-                            </div>
-                            <img className="bg-decoration" src={decoration} alt="vision"/>
+                            <MainPage/>
                         </Route>
                         <Route exact path="/comics">
-                            <AppBanner/>
-                            <ComicsList onComicsSelected={onComicsSelected} selectedId={selectedComics}/>
+                            <ComicsPage/>
                         </Route>
                     </Switch>
                 </main>
---
+++ src/components/pages/ComicsPage.js
@@ -0,0 +1,22 @@
+import { useState } from "react";
+
+import ComicsList from "../comicsList/ComicsList";
+import AppBanner from "../appBanner/AppBanner";
+
+const ComicsPage = () => {
+
+    const [selectedComics, setComics] = useState(null);
+
+    const onComicsSelected = (id) => {
+        setComics(id)
+    }
+
+    return(
+        <>
+            <AppBanner/>
+            <ComicsList onComicsSelected={onComicsSelected} selectedId={selectedComics}/>
+        </>
+    )
+}
+
+export default ComicsPage;
