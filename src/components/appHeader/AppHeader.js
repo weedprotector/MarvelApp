cd3198a4cfa570981811d2f4b30495112b47e935
Mikhail <weedprotector@gmail.com>
2022-10-26 15:12:09 +0500

    fix routing bug

--- src/components/appHeader/AppHeader.js
+++ src/components/appHeader/AppHeader.js
@@ -12,7 +12,6 @@ const AppHeader = () => {
             <nav className="app__menu">
                 <ul>
                     <li><NavLink 
-                        end 
                         style={({isActive}) => ({color: isActive ? '#9f0013' : 'inherit'})}
                         to={'/MarvelApp'}>Characters</NavLink></li>
                     /
