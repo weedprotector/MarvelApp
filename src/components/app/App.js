6e40682d5b5c19f79ffcf43c27267b6d32f2ffc4
Mikhail <weedprotector@gmail.com>
2022-10-27 17:26:46 +0500

    add lazy imports

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,27 +1,32 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { lazy, Suspense} from "react";
 
-import AppHeader from "../appHeader/AppHeader";
-import MainPage from "../pages/MainPage";
-import ComicsPage from "../pages/ComicsPage";
-import Page404 from "../pages/404";
-import SingleComicPage from "../pages/SingleComicPage"
+import Spinner from "../spinner/Spinner";
+import AppHeader from "../appHeader/AppHeader"
+
+const MainPage = lazy(() => import("../pages/MainPage"));
+const ComicsPage = lazy(() => import("../pages/ComicsPage"));
+const Page404 = lazy(() => import("../pages/404"));
+const SingleComicPage = lazy(() => import("../pages/SingleComicPage"));
 
 const App = () => {
 
     return (
-        <Router>
-            <div className="app">
-                <AppHeader/>
-                <main>
-                    <Routes>
-                        <Route path="/MarvelApp" element={<MainPage/>}/>
-                        <Route path="/comics" element={<ComicsPage/>}/>
-                        <Route path="/comics/:comicId" element={<SingleComicPage/>}/>
-                        <Route path="*" element={<Page404/>}/>
-                    </Routes>
-                </main>
-            </div>
-        </Router>
+        <Suspense fallback={<Spinner/>}>
+            <Router>
+                <div className="app">
+                    <AppHeader/>
+                    <main>
+                        <Routes>
+                            <Route path="/MarvelApp" element={<MainPage/>}/>
+                            <Route path="/comics" element={<ComicsPage/>}/>
+                            <Route path="/comics/:comicId" element={<SingleComicPage/>}/>
+                            <Route path="*" element={<Page404/>}/>
+                        </Routes>
+                    </main>
+                </div>
+            </Router>
+        </Suspense>
     )
 }
 
